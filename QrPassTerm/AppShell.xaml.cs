02975ae07edccd3b0d06ca71c9be5468bb15c8fa
Bora <Bora@DESKTOP-S7P5APN>
2022-11-22 01:43:51 +0600

    Добавьте файлы проекта.

---
+++ QrPassTerm/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using QrPassTerm.ViewModels;
+using QrPassTerm.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace QrPassTerm
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}

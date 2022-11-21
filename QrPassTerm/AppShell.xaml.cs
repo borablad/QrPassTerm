8a21079b6361d52a79f142d84dad55d07cc57b2c
Bora <Bora@DESKTOP-S7P5APN>
2022-11-22 03:15:07 +0600

    подключил несколько библиотек

--- QrPassTerm/AppShell.xaml.cs
+++ QrPassTerm/AppShell.xaml.cs
@@ -11,8 +11,8 @@ namespace QrPassTerm
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            //Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)

504ebcbfb45b9fbc42faa8ca140abcf0f58cc47c
Bora_blad <96587810+borablad@users.noreply.github.com>
2023-01-09 17:33:26 +0600

    settings in login

--- QrPassTerm/ViewModels/LoginViewModel.cs
+++ QrPassTerm/ViewModels/LoginViewModel.cs
@@ -15,12 +15,33 @@ namespace QrPassTerm.ViewModels
 
         [ObservableProperty]
         private string uName, pass;
+        [ObservableProperty]
+        private bool systemTheme, lightTheme, darkTheme, isExpanded;
+        [ObservableProperty]
+        private string userThem;
+
+        public string SchemeM { get { return Sheme; } set { Sheme = value; } }
+        public string PortM { get { return Port; } set { Port = value; } }
+        public string HostM { get { return HostUrl; } set { HostUrl = value; } }
+
+        public int userTheme
+        {
+            get => Preferences.Get("CastTheme", 0);
+            set
+            {
+                Preferences.Set("CastTheme", value);
+                OnPropertyChanged(nameof(userTheme));
+            }
+        }
         public LoginViewModel()
         {
 
         }
         internal async void onAppering()
         {
+            if (userTheme == 2) DarkTheme = true;
+            else if (userTheme == 1) LightTheme = true;
+            else SystemTheme = true;
 
             if (string.IsNullOrWhiteSpace(UserName) && string.IsNullOrWhiteSpace(Password)) return;
             UName = UserName;
@@ -79,6 +100,50 @@ namespace QrPassTerm.ViewModels
             IsBusy = false;
             await Shell.Current.GoToAsync($"{nameof(MainPage)}");
         }
+
+        [RelayCommand] // Смена темы
+        public void ThemeSelectionChanged(string parm)
+        {
+            int preferens = int.Parse(parm);
+            userTheme = preferens;
+            switch (preferens)
+            {
+                case 2:
+                    {
+
+                        Xamarin.Forms.Application.Current.UserAppTheme = OSAppTheme.Dark;
+                        UserThem = "Тёмная";
+                        SystemTheme = false;
+                        LightTheme = false;
+
+
+
+
+                        break;
+                    }
+                case 1:
+                    {
+                        Xamarin.Forms.Application.Current.UserAppTheme = OSAppTheme.Light;
+                        UserThem = "Светлая";
+                        SystemTheme = false;
+                        DarkTheme = false;
+
+
+                        break;
+                    }
+
+                default:
+                    {
+                        Xamarin.Forms.Application.Current.UserAppTheme = OSAppTheme.Unspecified;
+                        UserThem = "Системная";
+                        LightTheme = false;
+                        DarkTheme = false;
+
+                        break;
+                    }
+            }
+        }
+
         private void SaveUserDadta()
         {
             UserName = UName;
